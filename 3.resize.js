2a616a4c839afcc8871a40afe2e12c891d66b244
sainik <42866144+Sainik11@users.noreply.github.com>
2020-11-07 21:04:56 +0530

    Add files via upload

---
+++ 3.resize.js
@@ -0,0 +1,8 @@
+function getSize()
+{
+var width = document.documentElement.clientWidth;
+var height = document.documentElement.clientHeight;
+        
+
+ document.getElementById('wid-hyt').innerHTML = "<h1>Width: " + width + " and Height: " + height + "</h1>";
+}
